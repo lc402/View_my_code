8501719aa5e0dc8d534179336004b79ec344e2f4
liucx <l409688454@126.com>
2017-04-09 22:39:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/liucx/view_my/utils/UiUtils.java
@@ -0,0 +1,33 @@
+
+package com.example.liucx.view_my.utils;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
+
+public class UiUtils {
+
+    static public int getScreenWidthPixels(Context context) {
+        DisplayMetrics dm = new DisplayMetrics();
+        ((WindowManager) context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay()
+                .getMetrics(dm);
+        return dm.widthPixels;
+        //this is my test
+    }
+
+    static public int dipToPx(Context context, int dip) {
+        return (int) (dip * getScreenDensity(context) + 0.5f);
+    }
+
+    static public float getScreenDensity(Context context) {
+        try {
+            DisplayMetrics dm = new DisplayMetrics();
+            ((WindowManager) context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay()
+                    .getMetrics(dm);
+            return dm.density;
+        } catch (Exception e) {
+            return DisplayMetrics.DENSITY_DEFAULT;
+        }
+    }
+
+}
